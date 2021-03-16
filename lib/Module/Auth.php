a6fefc52a4b7a9342d2d82fbf8c08d8cbe336033
openthc <code@openthc.com>
2021-03-16 15:40:25 +0000

    apache config

--- lib/Module/Auth.php
+++ lib/Module/Auth.php
@@ -10,15 +10,7 @@ class Auth extends \OpenTHC\Module\Base
 	function __invoke($a)
 	{
 		$a->map(['GET','POST'], '/open', '\OpenTHC\Bong\Controller\Auth\Open');
-		$a->map(['GET','POST'], '/ping', function($REQ, $RES, $ARG) {
-			// return _from_cre_file('ping.php', $RES, $ARG);
-			$ret = [
-				'data' => $_SESSION,
-				'meta' => [],
-			];
-			__ksort_r($ret);
-			return $RES->withJSON($ret, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-		});
+		$a->map(['GET','POST'], '/ping', 'OpenTHC\Controller\Auth\Ping');
 		$a->get('/shut', 'OpenTHC\Controller\Auth\Shut');
 
 	}
