b175806e90252384707d73e3b1b95f9ab1789e71
openthc <code@openthc.com>
2022-11-21 20:00:09 +0000

    Lot CRUD

--- lib/CRE/ccrs/lot/create.php
+++ lib/CRE/ccrs/lot/create.php
@@ -1,6 +1,57 @@
 <?php
 /**
- *
+ * Inventory Create
  *
  * SPDX-License-Identifier: MIT
  */
+
+$dbc = $REQ->getAttribute('dbc');
+
+if (empty($_POST['product']) && !empty($_POST['product_id'])) {
+	$_POST['product'] = [
+		'id' => $_POST['product_id'],
+	];
+}
+if (empty($_POST['variety']) && !empty($_POST['variety_id'])) {
+	$_POST['variety'] = [
+		'id' => $_POST['variety_id'],
+	];
+}
+if (empty($_POST['section']) && !empty($_POST['section_id'])) {
+	$_POST['section'] = [
+		'id' => $_POST['section_id'],
+	];
+}
+
+
+$rec = [
+	'id' => _ulid(),
+	'license_id' => $_SERVER['HTTP_OPENTHC_LICENSE'],
+	'data' => json_encode($_POST),
+	// 'qty' => $_POST['qty'],
+	// 'variety_id' => $_POST['variety_id'],
+	// 'product_id' => $_POST['product_id'],
+];
+
+$ret = $dbc->insert('lot', $rec);
+
+return $RES->withJSON([
+	'data' => $rec,
+	'meta' => [],
+], 201);
+
+// Array
+// (
+//     [source] => Array
+//         (
+//             [0] => Array
+//                 (
+//                     [id] => 01FY7P6SXXTCY743
+//                     [qty] => 100.00
+//                 )
+//         )
+//     [product_id] => 01G81R70JTF6KHA43Z8FY3HGKC
+//     [qty] => 100
+//     [variety] => WAJWPP.ST6ZSN
+//     [section] => 01G899GGB0QWSG6WZ2APV9ECNB
+// )
--- lib/CRE/ccrs/lot/search.php
+++ lib/CRE/ccrs/lot/search.php
@@ -1,6 +1,40 @@
 <?php
 /**
- *
+ * Inventory Search
  *
  * SPDX-License-Identifier: MIT
  */
+
+$dbc = $REQ->getAttribute('dbc');
+
+// $res = $dbc->fetchAll("SELECT id, hash, updated_at, data->'result' AS result FROM section ORDER BY updated_at DESC");
+$sql = 'SELECT * FROM lot {WHERE} ORDER BY updated_at DESC';
+// $sql = 'SELECT id, stat, hash, updated_at FROM section {WHERE} ORDER BY updated_at DESC';
+
+$sql_param = [];
+$sql_where = [];
+
+// $sql_where[] = 'license_id = :l0';
+// $sql_param[':l0'] = $_SESSION['License']['id'];
+
+if ( ! empty($_GET['q'])) {
+	$sql_where[] = 'data::text LIKE :q23';
+	$sql_param[':q23'] = sprintf('%%%s%%', $_GET['q']);
+}
+
+if (count($sql_where)) {
+	$sql_where = implode(' AND ', $sql_where);
+	$sql = str_replace('{WHERE}', sprintf(' WHERE %s', $sql_where), $sql);
+} else {
+	$sql = str_replace('{WHERE}', '', $sql);
+}
+
+$res = $dbc->fetchAll($sql, $sql_param);
+$res['sql'] = $sql;
+
+return $RES->withJSON($res, 200, JSON_PRETTY_PRINT);
+
+// $dbc = $REQ->getAttribute('dbc');
+// $res = $dbc->fetchAll('SELECT id, hash, updated_at FROM lot ORDER BY updated_at DESC');
+
+// return $RES->withJSON($res, 200, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
