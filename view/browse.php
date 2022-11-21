3a4d6e27c96b43f41a4116d4c5d3f855d498e865
openthc <code@openthc.com>
2022-11-21 22:44:10 +0000

    Fix Badge Style

--- view/browse.php
+++ view/browse.php
@@ -1,6 +1,8 @@
 <?php
 /**
  *
+ *
+ * SPDX-License-Identifier: MIT
  */
 
  ?>
@@ -45,27 +47,27 @@
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/auth/open?r=%2Fbrowse">/auth/open</a> - Authenticate</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/auth/ping">/auth/ping</a> - Authentication and Session Information</div>
 	<div>
-		<span class="badge badge-primary">OpenTHC</span>
+		<span class="badge bg-primary">OpenTHC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/auth/shut">/auth/shut</a> - Close Session</div>
 	<div>
-		<span class="badge badge-primary">OpenTHC</span>
+		<span class="badge bg-primary">OpenTHC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/system">/system</a> - View System Information</div>
 	<div>
-		<span class="badge badge-primary">OpenTHC</span>
+		<span class="badge bg-primary">OpenTHC</span>
 	</div>
 </li>
 </ul>
@@ -111,78 +113,78 @@ printf($fmt, 'sync-time-omega', $t1);
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/company">/company</a> - Your Company Information</div>
 	<div>
-		<span class="badge badge-dark">system</span>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-secondary">LeafData</span>
-		<span class="badge badge-secondary">METRC</span>
+		<span class="badge bg-dark">system</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-secondary">LeafData</span>
+		<span class="badge bg-secondary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/contact">/contact</a> - Contacts, Drivers, Employees</div>
 	<div>
-		<span class="badge badge-dark">system</span>
-		<span class="badge badge-warning">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-secondary">METRC</span>
+		<span class="badge bg-dark">system</span>
+		<span class="badge bg-warning">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-secondary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/license-type">/license-type</a> - List all License Information</div>
 	<div>
-		<span class="badge badge-dark">system</span>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-dark">system</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/license">/license</a> - Your License Information</div>
 	<div>
-		<span class="badge badge-dark">system</span>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-dark">system</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/product-type">/product-type</a> - System Defined Product Type Details</div>
 	<div>
-		<span class="badge badge-dark">system</span>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-dark">system</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/product">/product</a> - Product Details</div>
 	<div>
-		<span class="badge badge-warning">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-warning">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/variety">/variety</a> - Variety (aka: Cultivar, Strains)</div>
 	<div>
-		<span class="badge badge-warning">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-warning">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/vehicle">/vehicle</a> - Vehicles</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-warning">LeafData</span>
-		<span class="badge badge-secondary">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-warning">LeafData</span>
+		<span class="badge bg-secondary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/section">/section</a> - an Area or Room or Zone</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 </ul>
@@ -199,9 +201,9 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div> <a href="/batch">/batch</a> - Batch Details</div>
 	<div>
-		<span class="badge badge-secondary">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-secondary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 </ul>
@@ -216,17 +218,17 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div> <a href="/crop">/crop</a> - Crop Details</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div> <a href="/crop/collect">/crop/collect</a> - Plant Collection Details</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 </ul>
@@ -241,25 +243,25 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/lot">/lot</a> - Inventory Lot Details</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/lot/history">/lot/history</a> - Inventory Lot History (Adjustment) Details</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/disposal">/disposal</a> - Inventory Waste and Disposal Details</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-secondary">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-secondary">METRC</span>
 	</div>
 </li>
 </ul>
@@ -274,9 +276,9 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/lab">/lab</a> - Inventory Lab Samples and Results</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 </ul>
@@ -290,25 +292,25 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/b2b/outgoing">/b2b/outgoing</a> - B2B Sales</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/b2b/incoming">/b2b/incoming</a> - B2B Sales to Accept</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/b2b/rejected">/b2b/rejected</a> - Rejects/Returns</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-warning">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-warning">METRC</span>
 	</div>
 </li>
 </ul>
@@ -323,9 +325,9 @@ Notably in LeafData the Batch also contains plant collection details.
 <li class="list-group-item d-flex justify-content-between align-items-center">
 	<div><a href="/b2c">/b2c</a> - B2C Sales</div>
 	<div>
-		<span class="badge badge-success">BioTrack</span>
-		<span class="badge badge-success">LeafData</span>
-		<span class="badge badge-success">METRC</span>
+		<span class="badge bg-primary">BioTrack</span>
+		<span class="badge bg-primary">LeafData</span>
+		<span class="badge bg-primary">METRC</span>
 	</div>
 </li>
 </ul>
