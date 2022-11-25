2b23bb8bbccf142470669f6547c555376370b204
openthc <code@openthc.com>
2022-11-25 03:41:52 +0000

    Refactor to only one file per upload

--- bin/cre-ccrs-push.php
+++ bin/cre-ccrs-push.php
@@ -2,9 +2,11 @@
 <?php
 /**
  * Use Curl to upload to the CCRS site
- * Get the cookies from var/
  *
  * SPDX-License-Identifier: MIT
+ *
+ * Get the cookies from var/
+ * Upload the files one at a time, was some transactional lock like issues with bulk
  */
 
 use OpenTHC\Bong\CRE;
@@ -13,10 +15,14 @@ require_once(__DIR__ . '/../boot.php');
 
 $dbc = _dbc();
 
-$file_list = _read_file_list($argv);
+$dtz = new \DateTimeZone( \OpenTHC\Config::get('cre/usa/wa/ccrs/tz'));
 
-$cookie_file0 = sprintf('%s/var/ccrs-cookies.json', APP_ROOT);
-$cookie_list0 = json_decode(file_get_contents($cookie_file0), true);
+$cookie_file = sprintf('%s/var/ccrs-cookies.json', APP_ROOT);
+if ( ! is_file($cookie_file)) {
+	echo "Cannot find Cookie File\n";
+	exit(1);
+}
+$cookie_list0 = json_decode(file_get_contents($cookie_file), true);
 foreach ($cookie_list0 as $c) {
 	if ('cannabisreporting.lcb.wa.gov' == $c['domain']) {
 		$cookie_list1[] = sprintf('%s=%s', $c['name'], $c['value']);
@@ -24,144 +30,203 @@ foreach ($cookie_list0 as $c) {
 }
 sort($cookie_list1);
 
-// Get to Verify Access and get RVT
-$req = __curl_init('https://cannabisreporting.lcb.wa.gov/');
-// curl_setopt($req, CURLOPT_VERBOSE, true);
-curl_setopt($req, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
-curl_setopt($req, CURLOPT_HTTPHEADER, [
-	'accept: text/html',
-	'authority: cannabisreporting.lcb.wa.gov',
-	sprintf('cookie: %s', implode('; ', $cookie_list1)),
-	'origin: https://cannabisreporting.lcb.wa.gov',
-	'referer: https://cannabisreporting.lcb.wa.gov/',
-]);
-$res = curl_exec($req);
-$inf = curl_getinfo($req);
-if ('200' != $inf['http_code']) {
-	echo "Not Authenticated\n";
-	exit(1);
-}
+$src_file_list = _read_file_list($argv);
 
-// I hate parsing HTML w/regex
-$rvt_code = preg_match('/<input name="__RequestVerificationToken" type="hidden" value="([^"]+)" \/>/', $res, $m) ? $m[1] : null;
-if (empty($rvt_code)) {
-	echo "Cannot find RVT\n";
-	exit(1);
-}
+foreach ($src_file_list as $src_file) {
 
-$req_file_list = [];
+	sleep(1); // to give their system a little bit of time to rest
 
-$part_mark = '----WebKitFormBoundaryAAAA8cKhBUv35ObB';
-$post = [];
+	$csrf_code = _get_main_page($cookie_list1);
 
-foreach ($file_list as $f) {
+	$mark = '----WebKitFormBoundaryAAAA8cKhBUv35ObB';
+	$post = [];
 
-	$src_data = file_get_contents($f);
+	$src_data = file_get_contents($src_file);
 
-	$post[] = sprintf('--%s', $part_mark);
-	$post[] = sprintf('content-disposition: form-data; name="files"; filename="%s"', basename($f));
+	// Fix Name on Upload
+	$src_name = basename($src_file);
+	if (preg_match('/(\w+_\w+)_01\w{24}/', $src_name, $m)) {
+		$dt0 = new \DateTime('now', $dtz);
+		$src_name = sprintf('%s_%s.csv', $m[1], $dt0->format('Ymd\TGisv'));
+	}
+
+	$post[] = sprintf('--%s', $mark);
+	$post[] = sprintf('content-disposition: form-data; name="files"; filename="%s"', $src_name);
 	// $post[] = 'content-transfer-encoding: binary';
 	$post[] = 'content-type: text/csv';
 	$post[] = '';
 	$post[] = $src_data;
 
-}
+	// Username
+	$post[] = sprintf('--%s', $mark);
+	$post[] = 'content-disposition: form-data; name="username"';
+	$post[] = '';
+	$post[] = \OpenTHC\Config::get('cre/usa/wa/ccrs/username');
 
-// Username
-$post[] = sprintf('--%s', $part_mark);
-$post[] = 'content-disposition: form-data; name="username"';
-$post[] = '';
-$post[] = 'code@openthc.com';
+	// RVT
+	$post[] = sprintf('--%s', $mark);
+	$post[] = 'content-disposition: form-data; name="__RequestVerificationToken"';
+	$post[] = '';
+	$post[] = $csrf_code; // Where to get this text?
 
-// RVT
-$post[] = sprintf('--%s', $part_mark);
-$post[] = 'content-disposition: form-data; name="__RequestVerificationToken"';
-$post[] = '';
-$post[] = $rvt_code; // Where to get this text?
+	// Closer and Combine
+	$post[] = sprintf('--%s--', $mark);
+	$post = implode("\r\n", $post);
+	// echo "Request Length: ";
+	// echo strlen($post);
+	// echo "\n";
 
-// Closer and Combine
-$post[] = sprintf('--%s--', $part_mark);
-$post = implode("\r\n", $post);
-echo "Request Length: ";
-echo strlen($post);
-echo "\n";
+	$upload_html = _post_home_upload($cookie_list1, $mark, $post);
 
-$req = __curl_init('https://cannabisreporting.lcb.wa.gov/Home/Upload');
-// curl_setopt($req, CURLOPT_VERBOSE, true);
-curl_setopt($req, CURLOPT_STDERR, fopen('php://stderr', 'a'));
-curl_setopt($req, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
-curl_setopt($req, CURLOPT_POST, true);
-curl_setopt($req, CURLOPT_POSTFIELDS, $post);
-curl_setopt($req, CURLOPT_HTTPHEADER, [
-	'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	'accept-language: en-US,en;q=0.9',
-	'authority: cannabisreporting.lcb.wa.gov',
-	'cache-control: max-age=0',
-	sprintf('content-length: %d', strlen($post)),
-	sprintf('content-type: multipart/form-data; boundary=%s', $part_mark),
-	sprintf('cookie: %s', implode('; ', $cookie_list1)),
-	'origin: https://cannabisreporting.lcb.wa.gov',
-	'referer: https://cannabisreporting.lcb.wa.gov/',
-	'sec-ch-ua-mobile: ?0',
-	'sec-ch-ua-platform: "Linux"',
-	'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-	'sec-fetch-dest: document',
-	'sec-fetch-mode: navigate',
-	'sec-fetch-site: same-origin',
-	'sec-fetch-user: ?1',
-	'upgrade-insecure-requests: 1',
-]);
-$res = curl_exec($req);
-$inf = curl_getinfo($req);
-// print_r($inf);
+	// Parse Upload Timestamp
+	$upload_time = null;
+	if (preg_match('/(Your submission was received at .+ Pacific Time)/', $upload_html, $m)) {
 
-echo "Response Length: ";
-echo strlen($res);
-echo "\n";
-// echo "$res";
+		$upload_time = $m[1];
+		echo "Uploaded At: {$m[1]}\n";
 
-// Parse Upload Timestamp
-$res_time = null;
-if (preg_match('/(Your submission was received at .+ Pacific Time)/', $res, $m)) {
+		// Move Files
+		$new_file = sprintf('%s/var/ccrs-complete/%s', APP_ROOT, basename($src_file));
+		rename($src_file, $new_file);
 
-	$res_time = $m[1];
-	echo "Uploaded At: {$m[1]}\n";
-
-	// Remove Files
-	foreach ($file_list as $old_file) {
-
-		$new_file = sprintf('%s/var/ccrs-complete/%s', APP_ROOT, basename($old_file));
-		rename($old_file, $new_file);
-
+		// Now Save in log_audit (log_upload)
 		$src_data = file_get_contents($new_file);
 		$req_code = null;
 		$req_ulid = null;
+
 		if (preg_match('/(\w+ UPLOAD (01\w+)).+-canary-/', $src_data, $m)) {
 
 			$req_code = $m[1];
 			$req_ulid = $m[2];
 
-			$rec = [];
-			$rec['id'] = $req_ulid;
-			$rec['name'] = $req_code;
-			$rec['source_data'] = $src_data;
-			$dbc->insert('log_upload', $rec);
+			$chk = $dbc->fetchRow('SELECT id FROM log_upload WHERE id = :u0', [ ':u0' => $req_ulid ]);
+			if (empty($chk)) {
+				$rec = [];
+				$rec['id'] = $req_ulid;
+				$rec['license_id'] = '';
+				$rec['name'] = $req_code;
+				$rec['source_data'] = json_encode([
+					'name' => $src_name,
+					'data' => $src_data
+				]);
+				$rec['result_data'] = json_encode([
+					'@upload' => $upload_html,
+				]);
+				$dbc->insert('log_upload', $rec);
+			} else {
+
+				$sql = <<<SQL
+				UPDATE log_upload
+				SET updated_at = now(), result_data = coalesce(result_data, '[]'::jsonb) || :rd1::jsonb
+				WHERE id = :u0
+				SQL;
+
+				$arg = [
+					':u0' => $req_ulid,
+					':rd1' => json_encode([
+						'@upload' => $upload_html,
+					])
+				];
+				// var_dump($arg);
+				$dbc->query($sql, $arg);
+				// var_dump($req_ulid);
+				// exit;
+			}
 
 		} else {
-			echo "NO MATCH, Eval Canary LIne, RE_SUMBIT?\n";
+			echo "NO MATCH, eval canary line, RE_SUMBIT?\n";
 			echo "file: $new_file\n";
 		}
 
+	} else {
+		echo "No Match on Upload Stuff\n";
 	}
 
-} else {
-	echo "No Match on Upload Stuff\n";
 }
 
 exit(0);
 
+/**
+ *
+ */
+function _get_main_page($cookie_list1) : string
+{
+	// Get to Verify Access and get RVT
+	$req = __curl_init('https://cannabisreporting.lcb.wa.gov/');
+	// curl_setopt($req, CURLOPT_VERBOSE, true);
+	curl_setopt($req, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
+	curl_setopt($req, CURLOPT_HTTPHEADER, [
+		'accept: text/html',
+		'authority: cannabisreporting.lcb.wa.gov',
+		sprintf('cookie: %s', implode('; ', $cookie_list1)),
+		'origin: https://cannabisreporting.lcb.wa.gov',
+		'referer: https://cannabisreporting.lcb.wa.gov/',
+	]);
+	$res = curl_exec($req);
+	$inf = curl_getinfo($req);
+	if ('200' != $inf['http_code']) {
+		echo "!! Not Authenticated\n";
+		exit(1);
+	}
 
-function _read_file_list($source_list)
+	// I hate parsing HTML w/regex
+	$csrf_code = preg_match('/<input name="__RequestVerificationToken" type="hidden" value="([^"]+)" \/>/', $res, $m) ? $m[1] : null;
+	if (empty($csrf_code)) {
+		echo "!! Cannot find RVT\n";
+		exit(1);
+	}
+
+	return $csrf_code;
+
+}
+
+/**
+ * Do the Upload
+ */
+function _post_home_upload($cookie_list1, $mark, $post) : string
+{
+	$req = __curl_init('https://cannabisreporting.lcb.wa.gov/Home/Upload');
+	// curl_setopt($req, CURLOPT_VERBOSE, true);
+	curl_setopt($req, CURLOPT_STDERR, fopen('php://stderr', 'a'));
+	curl_setopt($req, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
+	curl_setopt($req, CURLOPT_POST, true);
+	curl_setopt($req, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($req, CURLOPT_HTTPHEADER, [
+		'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+		'accept-language: en-US,en;q=0.9',
+		'authority: cannabisreporting.lcb.wa.gov',
+		'cache-control: max-age=0',
+		sprintf('content-length: %d', strlen($post)),
+		sprintf('content-type: multipart/form-data; boundary=%s', $mark),
+		sprintf('cookie: %s', implode('; ', $cookie_list1)),
+		'origin: https://cannabisreporting.lcb.wa.gov',
+		'referer: https://cannabisreporting.lcb.wa.gov/',
+		// 'sec-ch-ua-mobile: ?0',
+		// 'sec-ch-ua-platform: "Linux"',
+		// 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+		// 'sec-fetch-dest: document',
+		// 'sec-fetch-mode: navigate',
+		// 'sec-fetch-site: same-origin',
+		// 'sec-fetch-user: ?1',
+		// 'upgrade-insecure-requests: 1',
+	]);
+
+	$res = curl_exec($req);
+	$inf = curl_getinfo($req);
+
+	echo "Response Length: ";
+	echo strlen($res);
+	echo "\n";
+	// echo "$res";
+
+	return $res;
+
+}
+
+/**
+ *
+ */
+function _read_file_list($source_list) : array
 {
 	$return_list = [];
 
