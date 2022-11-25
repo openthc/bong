12d4d6bb2f7b1c478b04efd4e573a800a585167e
openthc <code@openthc.com>
2022-11-25 21:29:39 +0000

    Improve CCRS variety create

--- lib/CRE/ccrs/variety/create.php
+++ lib/CRE/ccrs/variety/create.php
@@ -8,7 +8,9 @@
 $dbc = $REQ->getAttribute('dbc');
 
 $rec = [
-	'id' => _ulid(),
+	// CCRS uses Name as Primary Key
+	// Has limit of 50 characters
+	'id' => strtoupper(substr(trim($_POST['name']), 0, 50)),
 	'license_id' => '018NY6XC00L1CENSE000000000',
 	'name' => trim($_POST['name']),
 	'data' => [
@@ -21,12 +23,26 @@ $rec = [
 	]
 ];
 
+// Already have this one?
+$chk = $dbc->fetchOne('SELECT id FROM variety WHERE id = :s0', [
+	':s0' => $rec['id']
+]);
+
+if ( ! empty($chk)) {
+
+	return $RES->withJSON([
+		'data' => [
+			'id' => $chk,
+		],
+		'meta' => [],
+	], 200);
+
+}
+
 // $rec['hash'] = \OpenTHC\CRE\Base::recHash($rec);
 $rec['hash'] = sha1(json_encode($rec));
 $rec['data'] = json_encode($rec['data']);
 
-// Set some flag so we know we need to sync it?
-// Or Depend on updated_at?
 $ret = $dbc->insert('variety', $rec);
 
 return $RES->withJSON([
