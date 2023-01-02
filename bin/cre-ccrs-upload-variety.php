78830a535d582ee18c398557f38188324e864d33
openthc <code@openthc.com>
2023-01-02 18:48:15 +0000

    Handle CCRS Version

--- bin/cre-ccrs-upload-variety.php
+++ bin/cre-ccrs-upload-variety.php
@@ -12,43 +12,58 @@ $dbc = _dbc();
 $tz0 = new DateTimezone(\OpenTHC\Config::get('cre/usa/wa/ccrs/tz'));
 $cre_service_key = \OpenTHC\Config::get('cre/usa/wa/ccrs/service-key');
 
-
-// CCRS v2022-343
-// $license_id = array_shift($argv);
-// $License = _load_license($dbc, $license_id);
-
-// CCRS v2021-340
 $License = [];
-$License['id'] = \OpenTHC\Config::get('openthc/root/license/id');
-$License['company_id'] = \OpenTHC\Config::get('openthc/root/company/id');
-$License['name'] = '-system-';
-$License['code'] = '-system-';
 
+define('CRE_CCRS_VERSION', getenv('CRE_CCRS_VERSION'));
+switch (CRE_CCRS_VERSION) {
+	case '2022.343'	:
+		$license_id = array_shift($argv);
+		$License = _load_license($dbc, $license_id);
+		break;
+	case '2021.340':
+	default:
+		$License = [];
+		$License['id'] = \OpenTHC\Config::get('openthc/root/license/id');
+		$License['company_id'] = \OpenTHC\Config::get('openthc/root/company/id');
+		$License['name'] = '-system-';
+		$License['code'] = '-system-';
+		break;
+}
 
 $req_ulid = _ulid();
 $csv_data = [];
-$csv_data[] = [ "VARIETY UPLOAD $req_ulid", '-canary-', '-canary-', '-canary-' ]; // v2021-340
-// $csv_data[] = [ '-canary-', "VARIETY UPLOAD $req_ulid", '-canary-', '-canary-', '-canary-' ]; // v2022-343
-$csv_name = sprintf('strain_%s_%s.csv', $cre_service_key, $req_ulid);
-$csv_head = explode(',', 'Strain,StrainType,CreatedBy,CreatedDate'); // v0
-// $csv_head = explode(',', 'LicenseNumber,Strain,StrainType,CreatedBy,CreatedDate'); // v1
+$csv_head = [];
+
+switch (CRE_CCRS_VERSION) {
+	case '2022.343':
+		$csv_data[] = [ '-canary-', "VARIETY UPLOAD $req_ulid", '-canary-', '-canary-', '-canary-' ];
+		$csv_head = explode(',', 'LicenseNumber,Strain,StrainType,CreatedBy,CreatedDate');
+		break;
+	case '2021.340':
+	default:
+		$csv_data[] = [ "VARIETY UPLOAD $req_ulid", '-canary-', '-canary-', '-canary-' ];
+		$csv_head = explode(',', 'Strain,StrainType,CreatedBy,CreatedDate');
+		break;
+}
+
+$csv_name = sprintf('Strain_%s_%s.csv', $cre_service_key, $req_ulid);
 $col_size = count($csv_head);
 $csv_temp = fopen('php://temp', 'w');
 
 
-
 $sql = <<<SQL
-SELECT *
+SELECT id, name
 FROM variety
-WHERE stat = 100 OR updated_at >= :d0
+WHERE license_id = :l0 AND stat = 100 OR updated_at >= :d0
 SQL;
 
 $res_variety = $dbc->fetchAll($sql, [
+	':l0' => $License['id'],
 	':d0' => '2022-06-01'
 ]);
 foreach ($res_variety as $variety) {
 	$csv_data[] = [
-		// $License['code'], // v1
+		$License['code'], // v1
 		substr($variety['name'], 0, 50)
 		, 'Hybrid'
 		, '-system-'
