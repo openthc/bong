fba572348017859176c51235fbf09ea0db173f54
openthc <code@openthc.com>
2023-01-30 21:31:35 +0000

    UPSERT w/hash

--- lib/CRE/ccrs/product/update.php
+++ lib/CRE/ccrs/product/update.php
@@ -7,32 +7,37 @@
  * For CCRS we just do an UPSERT every time
  */
 
-$dbc = $REQ->getAttribute('dbc');
+$_POST['name'] = trim($_POST['name']);
+$_POST['type'] = trim($_POST['type']);
 
-$sql = 'SELECT id, license_id FROM product WHERE id = :o1';
-$arg = [ ':o1' => $ARG['id'] ];
-$chk = $dbc->fetchRow($sql, $arg);
-if ( ! empty($chk['id'])) {
-
-	// License Conflict?
-	if ($chk['license_id'] != $_SESSION['License']['id']) {
-		return $RES->withJSON([
-			'data' => null,
-			'meta' => [
-				'detail' => 'Access Denied'
-			],
-		], 409);
-	}
-
+// Name Check
+if (empty($_POST['name'])) {
+	return $RES->withJSON([
+		'data' => null,
+		'meta' => [
+			'note' => 'Invalid Product Name [CPU-033]'
+		],
+	], 400);
 }
 
+// Type
+if (empty($_POST['type'])) {
+	return $RES->withJSON([
+		'data' => null,
+		'meta' => [
+			'note' => 'Invalid Product Type [CPU-044]'
+		],
+	], 400);
+}
+
+$dbc = $REQ->getAttribute('dbc');
 
 // UPSERT IT
 $sql = <<<SQL
 INSERT INTO product (id, license_id, name, hash, data) VALUES (:o1, :l0, :n0, :h0, :d0)
-ON CONFLICT (id) DO
+ON CONFLICT (id, license_id) DO
 UPDATE SET updated_at = now(), stat = 100, name = :n0, hash = :h0, data = coalesce(product.data, '{}'::jsonb) || :d0
-WHERE product.id = :o1 AND product.license_id = :l0
+WHERE product.hash != :h0
 SQL;
 
 $arg = [
@@ -44,22 +49,34 @@ $arg = [
 		'@source' => $_POST
 	]),
 ];
-$arg[':h0'] = sha1($arg[':d0']);
+$arg[':h0'] = \OpenTHC\CRE\Base::objHash( [
+	'id' => $ARG['id'],
+	'name' => $_POST['name'],
+	'type' => $_POST['type'],
+	'package' => $_POST['package'],
+]);
+
 
 $ret = $dbc->query($sql, $arg);
 if (1 == $ret) {
-	return $RES->withJSON([
-		'data' => [
-			'id' => $ARG['id'],
-			'name' => $_POST['name']
-		],
-		'meta' => $_POST,
-	]);
+
+	// $job_ulid = _ulid();
+
+	$R = \OpenTHC\Service\Redis::factory();
+	// $R->set(sprintf('/job/%s', $job_ulid), json_encode([
+	// 	'license' => $_SESSION['License']['id'],
+	// 	'object' => 'product',
+	// 	'object-id' => $ARG['id']
+	// ]));
+	// $R->rpush('/cre/ccrs/upload-queue', $job_ulid);
+	$R->set(sprintf('/license/%s/product', $_SESSION['License']['id']), 100);
+
 }
 
 return $RES->withJSON([
-	'data' => null,
-	'meta' => [
-		'detail' => 'Invalid Object',
+	'data' => [
+		'id' => $ARG['id'],
+		'name' => $_POST['name']
 	],
-], 500);
+	'meta' => [],
+]);
