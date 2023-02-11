46609997139f3bb1302830b620b40d8782f7ea94
openthc <code@openthc.com>
2023-02-11 22:33:25 +0000

    fix id create

--- lib/CRE/ccrs/lot/create.php
+++ lib/CRE/ccrs/lot/create.php
@@ -25,12 +25,9 @@ if (empty($_POST['section']) && !empty($_POST['section_id'])) {
 
 
 $rec = [
-	'id' => _ulid(),
+	'id' => $_POST['id'] ?: substr(_ulid(), 0, 16),
 	'license_id' => $_SESSION['License']['id'],
 	'data' => json_encode($_POST),
-	// 'qty' => $_POST['qty'],
-	// 'variety_id' => $_POST['variety_id'],
-	// 'product_id' => $_POST['product_id'],
 ];
 
 $ret = $dbc->insert('lot', $rec);
@@ -39,19 +36,3 @@ return $RES->withJSON([
 	'data' => $rec,
 	'meta' => [],
 ], 201);
-
-// Array
-// (
-//     [source] => Array
-//         (
-//             [0] => Array
-//                 (
-//                     [id] => 01FY7P6SXXTCY743
-//                     [qty] => 100.00
-//                 )
-//         )
-//     [product_id] => 01G81R70JTF6KHA43Z8FY3HGKC
-//     [qty] => 100
-//     [variety] => WAJWPP.ST6ZSN
-//     [section] => 01G899GGB0QWSG6WZ2APV9ECNB
-// )
