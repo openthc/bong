ba3ae6926a64a48696121128af6cb0f19465b3c7
openthc <code@openthc.com>
2023-02-28 05:46:16 +0000

    improve inventory pudate

--- lib/Controller/Inventory/Update.php
+++ lib/Controller/Inventory/Update.php
@@ -20,6 +20,7 @@ class Update extends \OpenTHC\Bong\Controller\Base\Update
 	{
 		$source_data = $_POST;
 		$source_data = \Opis\JsonSchema\Helper::toJSON($source_data);
+		$source_data->id = $ARG['id'];
 		$source_data->qty = floatval($source_data->qty);
 
 		$schema_spec = \OpenTHC\Bong\Inventory::getJSONSchema();
@@ -28,7 +29,7 @@ class Update extends \OpenTHC\Bong\Controller\Base\Update
 		// UPSERT IT
 		$sql = <<<SQL
 		INSERT INTO lot (id, license_id, name, hash, data) VALUES (:o1, :l0, :n0, :h0, :d0)
-		ON CONFLICT (id) DO
+		ON CONFLICT (id, license_id) DO
 		UPDATE SET updated_at = now(), stat = 100, name = EXCLUDED.name, hash = EXCLUDED.hash, data = coalesce(lot.data, '{}'::jsonb) || :d0
 		WHERE lot.id = EXCLUDED.id AND lot.license_id = EXCLUDED.license_id
 		SQL;
@@ -45,14 +46,36 @@ class Update extends \OpenTHC\Bong\Controller\Base\Update
 		$arg[':h0'] = \OpenTHC\CRE\Base::objHash($source_data);
 
 		$dbc = $REQ->getAttribute('dbc');
-		$ret = $dbc->query($sql, $arg);
+		$cmd = $dbc->prepare($sql);
+		$res = $cmd->execute($arg);
+		$hit = $cmd->rowCount();
+
+		$ret_code = 200;
+		switch ($hit) {
+			case 0:
+				// No INSERT or UPDATE
+				return $RES->withJSON([
+					'data' => $source_data,
+					'meta' => [],
+				], 202);
+				break;
+			case 1:
+				// Perfection
+				$ret = $cmd->fetch();
+				if (empty($ret['updated_at'])) {
+					$ret_code = 201;
+				}
+				break;
+			default:
+				throw new \Exception('Invalid Database State [CIA-073]');
+		}
 
 		$this->updateStatus();
 
 		return $RES->withJSON([
 			'data' => $source_data,
 			'meta' => [],
-		], 200);
+		], $ret_code);
 
 	}
 
