8b10f0af0d1269db68d07c06772064bd1f06c171
openthc <code@openthc.com>
2023-03-09 17:18:58 +0000

    just update var name

--- lib/Controller/Base/Update.php
+++ lib/Controller/Base/Update.php
@@ -27,10 +27,10 @@ class Update extends \OpenTHC\Controller\Base
 	 */
 	function updateStatus()
 	{
-		$R = \OpenTHC\Service\Redis::factory();
-		$R->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/stat', $this->_tab_name), 100);
-		$R->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/stat/time', $this->_tab_name), time());
-		$R->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/sync', $this->_tab_name), 100);
+		$rdb = \OpenTHC\Service\Redis::factory();
+		$rdb->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/stat', $this->_tab_name), 100);
+		$rdb->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/stat/time', $this->_tab_name), time());
+		$rdb->hset(sprintf('/license/%s', $_SESSION['License']['id']), sprintf('%s/sync', $this->_tab_name), 100);
 	}
 
 	/**
