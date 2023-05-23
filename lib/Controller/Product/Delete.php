d244dd46ca41f33232d2954988da493f37833966
openthc <code@openthc.com>
2023-05-23 15:17:11 +0000

    fix product delete

--- lib/Controller/Product/Delete.php
+++ lib/Controller/Product/Delete.php
@@ -14,34 +14,37 @@ class Delete extends \OpenTHC\Controller\Base
 {
 	use \OpenTHC\Traits\JSONValidator;
 
-	protected $_tab_name = 'section';
+	protected $_tab_name = 'product';
 
 	function __invoke($REQ, $RES, $ARG)
 	{
 		$dbc = $REQ->getAttribute('dbc');
 
 		// Object Exists?
-		$sql = 'SELECT id, license_id FROM product WHERE id = :p0';
-		$arg = [ ':p0' => $ARG['id'] ];
+		$sql = 'SELECT id, license_id FROM product WHERE license_id = :0 AND id = :p0';
+		$arg = [
+			':l0' => $_SESSION['License']['id'],
+			':p0' => $ARG['id']
+		];
 		$chk = $dbc->fetchRow($sql, $arg);
 		if (empty($chk['id'])) {
 			return $RES->withJSON([
 				'data' => null,
 				'meta' => [
-					'detail' => 'Not Found'
+					'note' => 'Not Found'
 				],
 			], 404);
 		}
 
 		// Access?
-		if ($chk['license_id'] != $_SESSION['License']['id']) {
-			return $RES->withJSON([
-				'data' => null,
-				'meta' => [
-					'detail' => 'Access Denied'
-				],
-			], 403);
-		}
+		// if ($chk['license_id'] != $_SESSION['License']['id']) {
+		// 	return $RES->withJSON([
+		// 		'data' => null,
+		// 		'meta' => [
+		// 			'note' => 'Access Denied'
+		// 		],
+		// 	], 403);
+		// }
 
 		// Delete
 		$sql = 'UPDATE product SET stat = 410 WHERE license_id = :l0 AND id = :p0';
@@ -63,7 +66,7 @@ class Delete extends \OpenTHC\Controller\Base
 		return $RES->withJSON([
 			'data' => null,
 			'meta' => [
-				'detail' => 'Invalid Object'
+				'note' => 'Invalid Object'
 			],
 		], 500);
 
