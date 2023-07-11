5f635633b60c64567638eee104b745377781e1c6
openthc <code@openthc.com>
2023-07-11 20:31:18 +0000

    fix meta response

--- lib/Middleware/CRE.php
+++ lib/Middleware/CRE.php
@@ -9,20 +9,22 @@ namespace OpenTHC\Bong\Middleware;
 
 class CRE extends \OpenTHC\Middleware\Base
 {
-
+	/**
+	 *
+	 */
 	public function __invoke($REQ, $RES, $NMW)
 	{
 		if (empty($_SESSION['cre'])) {
 			return $RES->withJSON([
 				'data' => null,
-				'meta' => [ 'detail' => 'Invalid Authentication State [BMC-017]' ],
+				'meta' => [ 'note' => 'Invalid Authentication State [BMC-017]' ],
 			], 403);
 		}
 
 		if (empty($_SESSION['cre-auth'])) {
 			return $RES->withJSON([
 				'data' => null,
-				'meta' => [ 'detail' => 'Invalid Authentication State [BMC-024]' ],
+				'meta' => [ 'note' => 'Invalid Authentication State [BMC-024]' ],
 			], 403);
 		}
 
