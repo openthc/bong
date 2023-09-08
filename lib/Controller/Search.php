eeb4bed9452700672fda9acb260d5c27c07008f7
openthc <code@openthc.com>
2023-09-08 15:22:31 +0000

    stub search

--- lib/Controller/Search.php
+++ lib/Controller/Search.php
@@ -13,17 +13,34 @@ class Search extends \OpenTHC\Controller\Base
 {
 	public $tab = null;
 
+	/**
+	 *
+	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
 		$dbc = $REQ->getAttribute('dbc');
 
-		$sql = <<<SQL
-		SELECT id, name FROM crop WHERE name LIKE :q1
-		UNION ALL
-		SELECT id, name FROM inventory WHERE name LIKE :q1
-		SQL;
+		$res = [];
 
-		$res = $dbc->fetchAll($sql);
+		$tab_list = [
+			'crop',
+			'inventory',
+			'b2b_outgoing_item',
+			'b2b_incoming_item',
+			'b2b_outgoing',
+			'b2b_incoming',
+		];
+
+		foreach ($tab_list as $tab) {
+			$sql = <<<SQL
+			SELECT id, name
+			FROM $tab
+			WHERE name LIKE :q1
+			   OR text::text LIKE :q1
+			SQL;
+
+			$res[$tab] = $dbc->fetchAll($sql, $arg);
+		}
 
 		$data = [];
 		$data['search_result'] = $res;
