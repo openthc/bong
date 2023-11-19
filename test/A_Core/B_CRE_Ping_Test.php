49c2ea97f9eaec59301aacce4391f49fc3049d2a
openthc <code@openthc.com>
2023-11-19 22:26:29 +0000

    improve ping test

--- test/A_Core/B_CRE_Ping_Test.php
+++ test/A_Core/B_CRE_Ping_Test.php
@@ -17,42 +17,26 @@ class B_CRE_Ping_Test extends \OpenTHC\Bong\Test\Base_Case
 	/**
 	 * @test
 	 */
-	public function load_cre()
-	{
-		$cre_list = \OpenTHC\CRE::getEngineList();
-
-		foreach ($cre_list as $cre_conf) {
-
-			$cre_conf['license'] = getenv('OPENTHC_TEST_LICENSE');
-			$cre_conf['license-key'] = getenv('OPENTHC_TEST_LICENSE_SECRET');
-
-			$cre = \OpenTHC\CRE::factory($cre_conf);
-
-			$this->assertNotEmpty($cre);
-			$this->assertTrue($cre instanceof \OpenTHC\CRE\Base);
-		}
-	}
-
-	/**
-	 * @test
-	 */
 	public function ping_cre()
 	{
-		$_SERVER['SERVER_NAME'] = getenv('OPENTHC_TEST_BASE');
-
 		$cre_list = \OpenTHC\CRE::getEngineList();
+
 		foreach ($cre_list as $cre_conf) {
 
-			// $cre_conf['service-sk'] = getenv('OPENTHC_TEST_BASE_SERVICE_KEY');
-			// $cre_conf['service-key'] = getenv('OPENTHC_TEST_BASE_SERVICE_KEY');
-			$cre_conf['contact'] = getenv('OPENTHC_TEST_CONTACT_ID');
 			$cre_conf['company'] = getenv('OPENTHC_TEST_COMPANY_ID');
+			$cre_conf['contact'] = getenv('OPENTHC_TEST_CONTACT_ID');
+			// $cre_conf['license'] = getenv('OPENTHC_TEST_LICENSE');
 			$cre_conf['license'] = getenv('OPENTHC_TEST_LICENSE_ID');
 			$cre_conf['license-key'] = getenv('OPENTHC_TEST_LICENSE_SECRET');
+			$cre_conf['service'] = getenv('OPENTHC_TEST_BASE_SERVICE_ID');
+			// $cre_conf['service-key'] = getenv('OPENTHC_TEST_BASE_SERVICE_KEY');
+			$cre_conf['service-sk'] = getenv('OPENTHC_TEST_BASE_SERVICE_KEY');
 
 			$cre = \OpenTHC\CRE::factory($cre_conf);
-			$cre->setLicense($cre_conf['license']);
+			$this->assertNotEmpty($cre);
+			$this->assertTrue($cre instanceof \OpenTHC\CRE\Base);
 
+			$res = $cre->setLicense($cre_conf['license']);
 			$res = $cre->ping();
 
 			$this->assertIsArray($res);
@@ -61,6 +45,7 @@ class B_CRE_Ping_Test extends \OpenTHC\Bong\Test\Base_Case
 			$this->assertArrayHasKey('data', $res, sprintf('Engine: %s', $cre_conf['id']));
 			$this->assertArrayHasKey('meta', $res, sprintf('Engine: %s', $cre_conf['id']));
 
+			break;
 		}
 	}
 
