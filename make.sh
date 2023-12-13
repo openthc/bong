5f075ee03074926f46da2678f62d605192d0bd00
openthc <code@openthc.com>
2023-12-13 15:39:57 +0000

    use common install helper

--- make.sh
+++ make.sh
@@ -18,7 +18,8 @@ case "$action" in
 # Install or Update the System
 install|update)
 
-	composer update --no-ansi --no-dev --no-progress --quiet --classmap-authoritative
+	composer install --no-ansi --no-dev --no-progress --quiet --classmap-authoritative
+
 	npm install
 
 	./make.sh vendor-web
@@ -28,35 +29,20 @@ install|update)
 # Get the CSS and JS Assets
 vendor-web)
 
+	. vendor/openthc/common/lib/lib.sh
+
+	copy_bootstrap
+	copy_fontawesome
+	copy_jquery
+
+
 	# lodash
 	mkdir -p webroot/vendor/lodash/
 	cp node_modules/lodash/lodash.min.js webroot/vendor/lodash/
 
-	# jquery
-	mkdir -p webroot/vendor/jquery/
-	cp node_modules/jquery/dist/jquery.min.js webroot/vendor/jquery/
-	cp node_modules/jquery/dist/jquery.min.map webroot/vendor/jquery/
-
-	# jquery-ui
-	mkdir -p webroot/vendor/jquery-ui/
-	cp node_modules/jquery-ui/dist/jquery-ui.min.js webroot/vendor/jquery-ui/
-	cp node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css webroot/vendor/jquery-ui/
-
-	# bootstrap
-	mkdir -p webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/css/bootstrap.min.css           webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/css/bootstrap.min.css.map       webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js      webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map  webroot/vendor/bootstrap/
-
 	# htmx
 	mkdir -p webroot/vendor/htmx
 	cp node_modules/htmx.org/dist/htmx.min.js webroot/vendor/htmx/
-
-	# font awesome
-	mkdir -p webroot/vendor/fontawesome/css webroot/vendor/fontawesome/webfonts
-	cp node_modules/@fortawesome/fontawesome-free/css/all.min.css webroot/vendor/fontawesome/css/
-	cp node_modules/@fortawesome/fontawesome-free/webfonts/* webroot/vendor/fontawesome/webfonts/
 
 	;;
 
