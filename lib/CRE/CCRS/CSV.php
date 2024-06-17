c4fdb952d798605e0ab543c1aa9cab310e9a991b
openthc <code@openthc.com>
2024-06-17 18:30:42 +0000

    drop canary lines

--- lib/CRE/CCRS/CSV.php
+++ lib/CRE/CCRS/CSV.php
@@ -15,8 +15,6 @@ class CSV
 
 	protected $_col_size;
 
-	protected $_req_data;
-
 	protected $_csv_data;
 
 	protected $_csv_head;
@@ -67,7 +65,7 @@ class CSV
 	 */
 	function getData($return_type='string')
 	{
-		$row_size = count($this->_csv_data) + 1;
+		$row_size = count($this->_csv_data);
 
 		$csv_temp = fopen('php://temp', 'w');
 
@@ -75,7 +73,6 @@ class CSV
 		\OpenTHC\CRE\CCRS::fputcsv_stupidly($csv_temp, array_values(array_pad([ 'SubmittedDate', date('m/d/Y') ], $col_size, '')));
 		\OpenTHC\CRE\CCRS::fputcsv_stupidly($csv_temp, array_values(array_pad([ 'NumberRecords', $row_size ], $col_size, '')));
 		\OpenTHC\CRE\CCRS::fputcsv_stupidly($csv_temp, array_values($this->_col_list));
-		\OpenTHC\CRE\CCRS::fputcsv_stupidly($csv_temp, $this->_req_data);
 		foreach ($this->_csv_data as $row) {
 			\OpenTHC\CRE\CCRS::fputcsv_stupidly($csv_temp, $row);
 		}
@@ -107,18 +104,15 @@ class CSV
 			case 'B2B/INCOMING':
 				$this->_csv_type = 'InventoryTransfer';
 				$this->_col_list = explode(',', 'FromLicenseNumber,ToLicenseNumber,FromInventoryExternalIdentifier,ToInventoryExternalIdentifier,Quantity,TransferDate,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', '-canary-', "B2B_INCOMING UPLOAD {$this->_csv_ulid}", '-canary-', 0, date('m/d/Y'), '-canary-', '-system-', date('m/d/Y'), '', '', 'UPDATE' ];
 				break;
 			case 'B2B/OUTGOING': // OpenTHC Name
 				$this->_csv_type = 'Sale';
 				$this->_col_list = explode(',', 'LicenseNumber,SoldToLicenseNumber,InventoryExternalIdentifier,PlantExternalIdentifier,SaleType,SaleDate,Quantity,UnitPrice,Discount,SalesTax,OtherTax,SaleExternalIdentifier,SaleDetailExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', '-canary-', "B2B_OUTGOING UPLOAD {$this->_csv_ulid}", '-canary-', '0', '', '-canary-', '-system-', date('m/d/Y'), '', '', 'UPDATE' ];
 				break;
 			// case 'B2B/OUTGOING/NOTICE': // OpenTHC Name
 			// case 'MANIFEST': // CCRS Name
 			// 	$this->_csv_type = 'Manifest';
 			// 	$this->_col_list =
-			// 	$this->_req_data =
 			// 	break;
 			// case 'B2C': // OpenTHC Name
 			// case 'SALES': // CCRS Name
@@ -127,34 +121,28 @@ class CSV
 			case 'PLANT':
 				$this->_csv_type = 'Plant';
 				$this->_col_list = explode(',', 'LicenseNumber,PlantIdentifier,Area,Strain,PlantSource,PlantState,GrowthStage,MotherPlantExternalIdentifier,HarvestDate,IsMotherPlant,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', "CROP UPLOAD {$this->_csv_ulid}", '-canary-', '-canary-', '-canary-', '-canary-', '-canary-', '-canary-', date('m/d/Y'), 'FALSE', '-canary-', 'OpenTHC', date('m/d/Y'), '' ,'', 'UPDATE' ];
 				break;
 			case 'INVENTORY':
 				$this->_csv_type = 'Inventory';
 				$this->_col_list = explode(',', 'LicenseNumber,Strain,Area,Product,InitialQuantity,QuantityOnHand,TotalCost,IsMedical,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', '-canary-', '-canary-', '-canary-', '0', '0', '0', 'FALSE', "INVENTORY UPLOAD {$this->_csv_ulid}", '-canary-', date('m/d/Y'), '-canary-', date('m/d/Y'), 'UPDATE' ];
 				break;
 			case 'INVENTORY/ADJUST':
 				$this->_csv_type = 'InventoryAdjustment';
 				$this->_col_list = explode(',', 'LicenseNumber,Strain,Area,Product,InitialQuantity,QuantityOnHand,TotalCost,IsMedical,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', '-canary-', '-canary-', '-canary-', '0', '0', '0', 'FALSE', "INVENTORY UPLOAD {$this->_csv_ulid}", '-canary-', date('m/d/Y'), '-canary-', date('m/d/Y'), 'UPDATE' ];
 				break;
 			case 'PRODUCT':
 				$this->_csv_type = 'Product';
 				$this->_col_list = explode(',', 'LicenseNumber,InventoryCategory,InventoryType,Name,Description,UnitWeightGrams,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', '-canary-', '-canary-', "PRODUCT UPLOAD {$this->_csv_ulid}", '', '0', '-canary-', '-canary-', date('m/d/Y'), '-canary-', date('m/d/Y'), 'UPDATE' ];
 				break;
 			case 'SECTION':
 			case 'AREA':
 				$this->_csv_type = 'Area';
 				$this->_col_list = explode(',', 'LicenseNumber,Area,IsQuarantine,ExternalIdentifier,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,Operation');
-				$this->_req_data = [ '-canary-', "SECTION UPLOAD {$this->_csv_ulid}", 'FALSE', '-canary-', '-canary-', date('m/d/Y'), '' ,'', 'DELETE' ];
 				break;
 			case 'VARIETY':
 			case 'STRAIN':
 				$this->_csv_type = 'Strain';
 				$this->_col_list = explode(',', 'LicenseNumber,Strain,StrainType,CreatedBy,CreatedDate');
-				$this->_req_data = [ '-canary-', "VARIETY UPLOAD {$this->_csv_ulid}", '-canary-', '-canary-', '-canary-' ];
 				break;
 			default:
 				throw new \Exception(sprintf('Invalid CSV Type "%s" [CCC-155]', $t));
