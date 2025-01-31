0cddaeb7f097daa4157b3f61fea5cece0d773879
openthc <code@openthc.com>
2025-01-31 20:47:36 +0000

    updated sync, handle Metrc2023

--- bin/sync.php
+++ bin/sync.php
@@ -10,30 +10,97 @@ use OpenTHC\Bong\CRE;
 
 require_once(__DIR__ . '/../boot.php');
 
-$opt = getopt('', [
-	'config:',
+openlog('openthc-bong', LOG_ODELAY | LOG_PID, LOG_LOCAL0);
+
+$doc = <<<DOC
+BONG CRE Sync
+Usage:
+	sync <command> [<command-options>...]
+
+Commands:
+	auth                  Authenticate to CRE
+	ping                  *auth
+	license-status        Show License Status
+	license-verify        Re-Init a License and try to Verify via magic Section
+	review                Review Data 400 Level Errors
+
+Options:
+	--company=<ID>
+	--license=<LICENSE_ID_LIST>
+	--object=<OBJECT_NAME_LIST>
+DOC;
+
+$res = Docopt::handle($doc, [
+	'exit' => false,
+	'help' => true,
+	'optionsFirst' => true,
 ]);
-$cfg_file = $opt['config'];
-if (empty($cfg_file)) {
-	echo "You Must supply a --config=<file>\n";
-	exit(1);
-}
-$cfg_file = realpath($cfg_file);
-if (empty($cfg_file)) {
-	echo "You Must supply a valid --config=<file>\n";
-	exit(1);
-}
-$cfg_data = file_get_contents($cfg_file);
-$_SESSION = json_decode($cfg_data, true);
-if (empty($_SESSION)) {
-	echo "You Must supply a valid --config=<file>, JSON parsing failed\n";
-	exit(1);
-}
+$cli_args = $res->args;
+// var_dump($cli_args);
+switch ($cli_args['<command>']) {
+case 'auth':
+case 'ping':
 
-$mdb = new \OpenTHC\Bong\Middleware\Database(null);
-$dbc = $mdb->connect();
+	$doc = <<<DOC
+	BONG CRE Sync Auth/Ping
+	Usage:
+		auth --company=<ID> --cre=<CRE> --cre-auth=<AUTH>
+	DOC;
 
-_set_option($dbc, 'sync-time-alpha', json_encode(date(\DateTime::RFC3339)));
+	$res = Docopt::handle($doc, [
+		'argv' => $cli_args['<command-options>'],
+		'exit' => true,
+		'help' => true,
+		'optionsFirst' => false,
+	]);
+	$cli_args = $res->args;
+	// var_dump($cli_args);
+
+	$dbc = _dbc();
+	$Company = $dbc->fetchRow('SELECT * FROM company WHERE id = :cp0', [
+		':cp0' => $cli_args['--company']
+	]);
+	if (empty($Company['id'])) {
+		throw new \Exception('Company Not Found', 404);
+	}
+
+	$cfg = \OpenTHC\CRE::getConfig($cli_args['--cre']);
+	$cfg['license-sk'] = $cli_args['--cre-auth'];
+	$cre = \OpenTHC\CRE::factory($cfg);
+
+	$res = $cre->license()->search();
+	foreach ($res['data'] as $License0) {
+		// FacilityId
+		echo "Found License: {$License0['Name']}\n";
+		$chk = $dbc->fetchRow('SELECT * FROM license WHERE company_id = :cp0 AND code = :l0', [
+			':cp0' => $Company['id'],
+			':l0' => $License0['License']['Number'],
+		]);
+		if (empty($chk)) {
+			$dbc->insert('license', [
+				'id' => _ulid(),
+				'company_id' => $Company['id'],
+				'code' => $License0['License']['Number'],
+				'name' => $License0['Name'],
+			]);
+		}
+	}
+
+	break;
+
+case 'license-status':
+case 'license-verify':
+case 'review':
+	throw new \Exception('Not Handled', 501);
+	break;
+case 'sync':
+	_bong_sync($cli_args['<command-options>']);
+	break;
+}
+// var_dump($cli_args);
+exit;
+
+// _set_option($dbc, 'sync-time-alpha', json_encode(date(\DateTime::RFC3339)));
 
 // Switch to Sub-Script
 switch ($_SESSION['cre']['engine']) {
@@ -53,6 +120,122 @@ switch ($_SESSION['cre']['engine']) {
 
 _set_option($dbc, 'sync-time-omega', json_encode(date(\DateTime::RFC3339)));
 
+/**
+ *
+ */
+function _bong_sync($argv)
+{
+	$doc = <<<DOC
+	BONG CRE Sync
+	Usage:
+		sync --company=<ID>  --cre=<CRE> --cre-auth=<AUTH> [ --object=LIST ] [--date-alpha=A ] [ --date-omega=DT ]
+	DOC;
+
+	$res = Docopt::handle($doc, [
+		'argv' => $argv,
+		'exit' => true,
+		'help' => true,
+		'optionsFirst' => false,
+	]);
+	$cli_args = $res->args;
+	// var_dump($cli_args);
+
+	$dbc = _dbc();
+
+	$Company = $dbc->fetchRow('SELECT * FROM company WHERE id = :cp0', [
+		':cp0' => $cli_args['--company']
+	]);
+	if (empty($Company['id'])) {
+		throw new \Exception('Company Not Found', 404);
+	}
+	$res_license = $dbc->fetchAll('SELECT * FROM license WHERE company_id = :cp0', [
+		':cp0' => $Company['id'],
+	]);
+
+
+	$cfg = \OpenTHC\CRE::getConfig($cli_args['--cre']);
+	$cfg['license-sk'] = $cli_args['--cre-auth'];
+	$cre = \OpenTHC\CRE::factory($cfg);
+
+	// $req = $cre->_curl_init('/unitsofmeasure/v2/active');
+	// $res = $cre->_curl_exec($req);
+	// var_dump($res);
+
+	// $req = $cre->_curl_init('/wastemethods/v2/');
+	// $res = $cre->_curl_exec($req);
+	// var_dump($res);
+
+	// Filter from CLI Args?
+	$res = $cre->license()->search();
+	// print_r($cre_license_list);
+	foreach ($res['data'] as $x) {
+		echo $x['License']['Number'];
+		echo ' ';
+		echo $x['Name'];
+		echo "\n";
+	}
+
+	foreach ($res_license as $License) {
+
+		$License['guid'] = $License['code'];
+		$cre->setLicense($License);
+
+		$cre_sync = new \OpenTHC\Bong\CRE\Metrc2023\Sync($cre, $dbc);
+		$res = $cre_sync->execute();
+		var_dump($res);
+
+	}
+
+	exit(0);
+
+	// $obj_list = [];
+	// $obj_list[] = 'license'; // /facilities/v2/
+	// $obj_lsit[] = 'harvests';
+	// // $obj_list[] = 'items';
+	// $obj_list[] = 'locations';
+	// $obj_list[] = 'plant';
+	// $obj_list[] = 'uom'; // '/unitsofmeasure/v2/active';
+	// foreach ($obj_list as $obj_stub) { // } => $obj_name) {
+	// 	if ( ! $sync->is_time_aware($obj_stub)) {
+	// 		echo "SYNC: $obj_stub\n";
+	// 		// _sync_object_data($dbc, $cre, $obj_stub);
+	// 	}
+	// }
+
+	// For the Dates
+	// $d0 = new DateTime();
+	// $d1 = new DateTime();
+	// if (!empty($opt['from'])) {
+	// 	$d0 = new DateTime($opt['from']);
+	// } else {
+	// 	$d0->sub(new DateInterval('P7D')); // A Week
+	// }
+	// if (!empty($opt['thru'])) {
+	// 	$d1 = new DateTime($opt['thru']);
+	// }
+
+	// $dL = clone $d0;
+	// while ($dL < $d1) {
+
+	// 	$st0 = $dL->format(DateTime::RFC3339);
+	// 	$dL->add(new DateInterval('PT24H'));
+	// 	$st1 = $dL->format(DateTime::RFC3339);
+
+	// 	// Set Date to View
+	// 	$sync->setTimeAlpha($st0);
+	// 	$sync->setTimeOmega($st1);
+
+	// 	foreach ($obj_list as $obj_stub => $obj_name) {
+	// 		echo "Sync: {$License['name']} / {$obj_stub} {$st0}\n";
+	// 		// _sync_object_data($dbc, $cre, $obj_stub);
+	// 	}
+	// }
+
+	// $Company['cre'];
+	// $cfg = \OpenTHC\CRE::getConfig('usa-mi');
+
+
+}
 
 /**
  * Table Name Mapper
--- lib/CRE/CCRS/Upload.php
+++ lib/CRE/CCRS/Upload.php
@@ -16,6 +16,60 @@ class Upload
 	protected string $obj;
 
 	/**
+	 * Use BONG API to Insert Outgoing CSV
+	 */
+	static function enqueue(array $License, string $csv_name, $csv_data)
+	{
+		$url_base = \OpenTHC\Config::get('openthc/bong/origin');
+
+		$cfg = array(
+			'base_uri' => $url_base,
+			'allow_redirects' => false,
+			'cookies' => false,
+			'http_errors' => false,
+			'verify' => false,
+		);
+		$api_bong = new \GuzzleHttp\Client($cfg);
+
+		$arg = [
+			'headers' => [
+				'content-name' => basename($csv_name),
+				'content-type' => 'text/csv',
+				'openthc-company' => $License['company_id'], // v0
+				'openthc-company-id' => $License['company_id'], // v1
+				'openthc-license' => $License['id'], // v0
+				'openthc-license-id' => $License['id'], // v1
+				'openthc-license-code' => $License['code'],
+				'openthc-license-name' => $License['name'],
+				'openthc-disable-update' => true,
+			],
+			'body' => $csv_data // this resource is closed by Guzzle
+		];
+
+		if ( ! empty($_SERVER['argv'])) {
+			$argv = implode(' ', $_SERVER['argv']);
+			if (strpos($argv, '--dump')) {
+				if (is_resource($arg['body'])) {
+					$arg['body'] = stream_get_contents($arg['body']);
+				}
+				var_dump($arg['headers']);
+				echo ">>>\n{$arg['body']}###\n";
+				return;
+			}
+		}
+
+		$res = $api_bong->post('/upload/outgoing', $arg);
+
+		$hrc = $res->getStatusCode();
+		$buf = $res->getBody()->getContents();
+		$buf = trim($buf);
+
+		echo "## BONG $csv_name = $hrc\n";
+		echo ">> $buf ..\n";
+
+	}
+
+	/**
 	 *
 	 */
 	function __construct($cfg)
@@ -47,7 +101,7 @@ class Upload
 		$max_age = 60 * 60 * 8; // 8 hours
 		switch ($tmp_stat) {
 		case 102:
-			$max_age = 60 * 30; // 30 minutes
+			$max_age = 60 * 60; // 60 minutes
 			break;
 		}
 
